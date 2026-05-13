ed4ff5df8c00bd8ccc5df99cb446b4a144f2cba3
massar0507 <bishamontenkeisuke5759@gmail.com>
2026-05-14 01:58:51 +0900

    feat: update site URL and base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-   // site: 'https://massar'
+   site: 'https://massar-57.github.io',
+   base: '/MyWebsite',
 });
